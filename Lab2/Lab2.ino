b28106e73c9ffae773a4716ed92cbce1614770a8
epeterson02 <erikpeterson335@gmail.com>
2025-01-23 14:56:41 -0600

    For Loop
    Added a for loop that compares i to timesToBlink variable and increments each time

--- Lab2/Lab2.ino
+++ Lab2/Lab2.ino
@@ -26,17 +26,14 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  int timesToBlink = 1;
-  timesToBlink = timesToBlink + 2 * 4;
-  if(timesToBlink > 4){
+  int timesToBlink = 2;
+  for(int i = 0; i <= timesToBlink; i ++){ //for loop compares i to timestoblink variable and increments
     digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(1000);                      // wait for a second
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
     delay(1000);   
   }
-  else{
-    digitalWrite(LED_BUILTIN, LOW);
-  }
+  
 }
 
 
