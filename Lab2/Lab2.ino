e0cdfa951569253be6fbf6c5236550789d4cb076
epeterson02 <erikpeterson335@gmail.com>
2025-01-23 15:01:13 -0600

    While loop
    The LED does not stop blinking because it never exits the loop and thus cannot do any other operations.

--- Lab2/Lab2.ino
+++ Lab2/Lab2.ino
@@ -27,12 +27,13 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   int timesToBlink = 2;
-  for(int i = 0; i <= timesToBlink; i ++){ //for loop compares i to timestoblink variable and increments
+  while(timesToBlink > 0){ //for loop compares i to timestoblink variable and increments
     digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(1000);                      // wait for a second
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
     delay(1000);   
   }
+  digitalWrite(LED_BUILTIN, HIGH);
   
 }
 
